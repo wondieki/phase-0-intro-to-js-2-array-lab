1c7166ecb0df490e4085fdcba548e707825a0305
Wilder <wkerubo28@gmail.com>
2023-04-17 22:58:58 +0300

    Completed Assignment

--- index.js
+++ index.js
@@ -1,1 +1,38 @@
 // Write your solution here!
+
+//Add the cat "Ralph" to the end of the array.
+const cats =["Milo", "Otis", "Garfield"];
+cats.push("Ralph");
+console.log(cats)
+
+//Add a cat "Bob" to the start of the array.
+const cats =["Milo", "Otis", "Garfield"];
+cats.unshift("Bob");
+console.log(cats)
+
+//Remove the last cat name
+const cats =["Milo", "Otis", "Garfield"];
+cats.pop("Garfield");
+console.log(cats)
+
+//Remove the first cat name
+const cats =["Milo", "Otis", "Garfield"];
+cats.shift("Milo");
+console.log(cats)
+
+
+//Add cat "Broom" to the last
+const cats =["Milo", "Otis", "Garfield"];
+cats.push("Broom");
+console.log(cats)
+
+
+//Add the cat "Arnold to be the first name"
+const cats =["Milo", "Otis", "Garfield"];
+cats.unshift("Arnold");
+console.log(cats)
+
+//Remove  the last cat
+const cats =["Milo", "Otis", "Garfield"];
+cats.pop("Garfield");
+console.log(cats)
