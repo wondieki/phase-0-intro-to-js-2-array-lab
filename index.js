90ebe57dcfbabf577b77e39e6d05519bfd3d2d8b
wondieki <wkerubo28@gmail.com>
2024-11-04 12:10:58 +0300

    Done

--- index.js
+++ index.js
@@ -1,38 +1,29 @@
-
-
-//Add the cat "Ralph" to the end of the array.
-const cats =["Milo", "Otis", "Garfield"];
-cats.push("Ralph");
-console.log(cats)
-
-//Add a cat "Bob" to the start of the array.
-const cats =["Milo", "Otis", "Garfield"];
-cats.unshift("Bob");
-console.log(cats)
-
-//Remove the last cat name from the array
-const cats =["Milo", "Otis", "Garfield"];
-cats.pop("Garfield");
-console.log(cats)
-
-//Remove the first cat name from the array
-const cats =["Milo", "Otis", "Garfield"];
-cats.shift("Milo");
-console.log(cats)
-
-
-//Add cat "Broom" to the last 
-const cats =["Milo", "Otis", "Garfield"];
-cats.push("Broom");
-console.log(cats)
-
-
-//Add the cat "Arnold to be the first name"
-const cats =["Milo", "Otis", "Garfield"];
-cats.unshift("Arnold");
-console.log(cats)
-
-//Remove  the last name from the array
-const cats =["Milo", "Otis", "Garfield"];
-cats.pop("Garfield");
-console.log(cats)
+let cats=["Milo","Otis","Garfield"];
+function destructivelyAppendCat(){
+    cats.push("Ralph");
+}
+function  destructivelyPrependCat(){
+    cats.unshift("Bob");
+}
+function destructivelyRemoveLastCat(){
+    cats.pop();
+}
+function destructivelyRemoveFirstCat(){
+    cats.shift();
+}
+function appendCat(){
+    let copyOfCats=[...cats, 'Broom']
+    return copyOfCats;
+}
+function prependCat(){
+   var newCats=['Arnold', ...cats]
+    return newCats;
+}
+function removeLastCat(){
+    let copyOfNewCats=cats.slice(0,-1)
+    return copyOfNewCats;
+}
+function removeFirstCat(){
+    var newCatsString=cats.slice(1)
+    return newCatsString;
+}
